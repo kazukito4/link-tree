a2abe4754d4c875900daaa0fc582d61bae946e02
kazukito4 <kazukazukito@gmail.com>
2025-11-07 18:06:08 +0900

    criei o cogico

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from dash import Dash
-
-
+import dash_bootstrap_components as dbc
 from layout.layout_heander import Layout
-app = Dash(__name__)
+
+
+app = Dash(__name__,external_stylesheets=[dbc.themes.DARKLY])
 app.layout = Layout.creat_layout()
 
 if __name__ == "__main__":
